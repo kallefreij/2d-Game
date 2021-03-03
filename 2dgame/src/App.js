26bfd3074f5d48ee1952f530df93563644f00e2d
David <Davidlingvall@gmail.com>
2021-03-03 20:51:07 +0100

    First

--- 2dgame/src/App.js
+++ 2dgame/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="scoreBoard">
+          <div class="score">
+            <span>1</span>
+            <span>:</span>
+            <span>2</span>
+          </div>
+        </div>
+        <canvas id="myCanvas" width="720" height="320"></canvas>
       </header>
     </div>
   );
